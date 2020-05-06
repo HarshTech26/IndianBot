128f30ac57bce8d1e7a7db8a08285f175cb4e426
HarshTech26 <58262783+HarshTech26@users.noreply.github.com>
2020-05-06 17:32:10 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -17,7 +17,7 @@ async def amireallyalive(alive):
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
                      "`Telethon version:` **6.0.9**\n`Python:` **3.7.4**\n"
                      "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
-                     "**Bot Creator:** [Anonymous](t.me/anon_cracker)\n"
-                     "**Co-Owner:** [PeruASF](t.me/anon_cracker2)\n\n"
+                     "**Bot Creator:** [🇮🇳Λησηумσυѕ🇮🇳](t.me/anon_cracker)\n"
+                     "**Co-Owner:** [Anonymous](t.me/anon_cracker2)\n\n"
                      "     [🇮🇳Deploy This Userbot🇮🇳](https://github.com/HarshTech26/IndianBot/)") 
 
